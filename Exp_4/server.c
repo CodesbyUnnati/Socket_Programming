7dd91ba48cb31d2d139a5c2c75b68601777ccec5
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2020-11-10 22:07:50 +0530

    Create server.c

---
+++ Exp_4/server.c
@@ -0,0 +1,55 @@
+#include<sys/socket.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+    int sd;
+    char buff[1024];
+    struct sockaddr_in cliaddr,servaddr;
+    socklen_t clilen;
+    clilen=sizeof(cliaddr);
+    sd=socket(AF_INET,SOCK_DGRAM,0);
+    if (sd<0)
+    {
+        perror ("Cannot open Socket");
+        exit(1);
+    }
+    bzero(&servaddr,sizeof(servaddr));
+    /*Socket address structure*/
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(5669);
+    /*Bind function assigns a local protocol address to the socket*/
+    if(bind(sd,(struct sockaddr*)&servaddr,sizeof(servaddr))<0)
+    {
+        perror("error in binding the port");
+        exit(1);
+    }
+    printf("%s","Server is Running...\n");
+    while(1)
+    {
+        bzero(&buff,sizeof(buff));
+        /*Read the message from the client*/
+        if(recvfrom(sd,buff,sizeof(buff),0,(struct sockaddr*)&cliaddr,&clilen)<0)
+        {
+            perror("Cannot rec data");
+            exit(1);
+        }
+        printf("Message is received \n %s",buff);
+        /*Sendto function is used to echo the message from server to client side*/
+        printf(" data IS: %s",buff);
+    }
+    close(sd);
+    return 0;
+}
+
+
+//COMMANDS
+/*~$ gcc Server.c -o Server
+~$ ./Server*/
